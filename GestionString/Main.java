0dd406e6dd17a231bef5fc28c085edee1e1cc1e3
damien vivier <vivdams@gmail.com>
2020-03-31 09:48:58 +0200

    suppression de  Accum (exos kata) commentaires dans GestionString

--- GestionString/Main.java
+++ GestionString/Main.java
@@ -5,8 +5,10 @@ public class Main {
     public static void main(String[] args) {
         String enMinuscule = "minusa et encore a ";
 
+//  Exercice 1 : transforme une chaîne de caracteres en majuscule
         System.out.println(enMinuscule.toUpperCase());
 
+//  Exercice 2 : compte le nombre de "a" dans une phrase
         int debut = -1;
         int nombreDeA = 0;
 //        System.out.println(enMinuscule.indexOf('a'));
@@ -17,14 +19,15 @@ public class Main {
         }
         System.out.println("Nombre de a : " + nombreDeA);
 
+//  Exercice 3 : transforme tout les "a" en "4"
         System.out.println(enMinuscule.replace('a', '4'));
 
-//  Exercice 4
+//  Exercice 4 : supprime les caractères compris entre le 5eme et le 10eme caractères
         String chaineReduite;
         chaineReduite = enMinuscule.substring(0, 4) + enMinuscule.substring(10, enMinuscule.length());
         System.out.println("chaineReduite = " + chaineReduite);
 
-//  Exercice 5
+//  Exercice 5 : transforme un entier en String
         int unEntier = 808;
         String unEntierEnString = "";
         unEntierEnString = String.valueOf(unEntier);
@@ -34,7 +37,7 @@ public class Main {
         unEntierEnString = Integer.toString(unEntier);
         System.out.println("unEntierEnString = " + unEntierEnString);
 
-//  Exercice 6
+//  Exercice 6 : transforme un String en entier
         unEntierEnString = "303";
         int unStringEnUnEntier;
         unStringEnUnEntier = Integer.valueOf(unEntierEnString);  // renvoi un Integer, pas un int
